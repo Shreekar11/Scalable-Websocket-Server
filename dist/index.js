ed141ab9247e6f6ad0aa87a48b4fa84634d839b1
Shreekar11 <shreekargade2004@gmail.com>
2025-03-21 21:13:11 +0530

    feat: websocket init

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,28 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+const subscriptions = {};
+wss.on("connection", function connection(ws) {
+    const id = Math.random().toString(36).substring(7);
+    subscriptions[id] = {
+        ws: ws,
+        rooms: [],
+    };
+    ws.on("message", function message(data) {
+        const parseMessage = JSON.parse(data.toString());
+        if (parseMessage.type === "subscribe") {
+            subscriptions[id].rooms.push(parseMessage.room);
+        }
+        if (parseMessage.type === "sendMessage") {
+            const message = parseMessage.message;
+            const roomId = parseMessage.roomId;
+            Object.keys(subscriptions).forEach((key) => {
+                const { ws, rooms } = subscriptions[key];
+                if (rooms.includes(roomId)) {
+                    ws.send(message);
+                }
+            });
+        }
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-import express from "express";
+import { WebSocket, WebSocketServer } from "ws";
 
-const app = express();
+const wss = new WebSocketServer({ port: 8080 });
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+const subscriptions: {
+  [key: string]: {
+    ws: WebSocket;
+    rooms: string[];
+  };
+} = {};
+
+wss.on("connection", function connection(ws) {
+  const id = Math.random().toString(36).substring(7);
+  subscriptions[id] = {
+    ws: ws,
+    rooms: [],
+  };
+
+  ws.on("message", function message(data) {
+    const parseMessage = JSON.parse(data.toString());
+    if (parseMessage.type === "subscribe") {
+      subscriptions[id].rooms.push(parseMessage.room);
+    }
+
+    if (parseMessage.type === "sendMessage") {
+      const message = parseMessage.message;
+      const roomId = parseMessage.roomId;
+
+      Object.keys(subscriptions).forEach((key) => {
+        const { ws, rooms } = subscriptions[key];
+        if (rooms.includes(roomId)) {
+          ws.send(message);
+        }
+      });
+    }
+  });
 });
